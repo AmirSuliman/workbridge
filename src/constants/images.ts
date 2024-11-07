0c77b6d55bbe822acc1906cbdc2264ed95f55e89
ubaidu647 <ubaiduu647@gmail.com>
2024-11-07 15:19:58 +0500

    resolve biuld issue

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,3 +1,8 @@
+// export const IMAGES = {
+//   dummyImage: require('@/src/assets/images/userImage.png').default,
+// };
+import dummyImage from '@/src/assets/images/userImage.png';
+
 export const IMAGES = {
-  dummyImage: require('@/src/assets/images/userImage.png').default,
+  dummyImage,
 };
