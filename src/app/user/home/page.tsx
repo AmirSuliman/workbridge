8214b7d301e072d8b0e0bf2cb93b223484fe522e
amirsuliman01 <amirsuliman01@gmail.com>
2025-02-04 18:36:35 +0500

    just a dummy save in user panel

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react'
+import React from 'react';
 import WhosOut from '@/components/WhosOut/WhosOut';
 import SingleAnnouncement from '@/components/SingleAnnouncement/SingleAnnouncement';
 import Link from 'next/link';
@@ -17,35 +17,35 @@ import Newpolicyupdate from './components/newpolicyupdate';
 
 const page = () => {
   return (
-    <div className='p-6'>
-    <main className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <WhosOut />
-      <div className='flex flex-col gap-5'>
-      <Evaluation/>
-      <Newpolicyupdate/>
-      </div>
-      <Celebrations />
-      <section className="bg-white rounded-xl border-[1px] border-[#E0E0E0] py-4 space-y-2">
-        <header className="px-4 flex items-center gap-4 justify-between">
-          <h1 className="flex items-center gap-4 font-semibold text-xl mb-4">
-            <HiSpeakerphone />
-            Announcements
-          </h1>
-          <Link href="/hr/announcements-&-policies/announcements">
-            <Button
-              name="See All"
-              icon={<PiArrowUpRightThin size={18} />}
-              bg="transparent"
-              textColor="black"
-            />
-          </Link>
-        </header>
-        <SingleAnnouncement />
-      </section>
-      <Training />
-      <NewEmployees />
-      <Companyinfo />
-    </main>
+    <div className="p-6">
+      <main className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <WhosOut />
+        <div className="flex flex-col gap-5">
+          <Evaluation />
+          <Newpolicyupdate />
+        </div>
+        <Celebrations />
+        <section className="bg-white rounded-xl border-[1px] border-[#E0E0E0] py-4 space-y-2">
+          <header className="px-4 flex items-center gap-4 justify-between">
+            <h1 className="flex items-center gap-4 font-semibold text-xl mb-4">
+              <HiSpeakerphone />
+              Announcements
+            </h1>
+            <Link href="/hr/announcements-&-policies/announcements">
+              <Button
+                name="See All"
+                icon={<PiArrowUpRightThin size={18} />}
+                bg="transparent"
+                textColor="black"
+              />
+            </Link>
+          </header>
+          <SingleAnnouncement />
+        </section>
+        <Training />
+        <NewEmployees />
+        <Companyinfo />
+      </main>
     </div>
   );
 };
