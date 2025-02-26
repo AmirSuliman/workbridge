96603cdd4d471edf4c393a1554280fa2e1d1404b
unknown <abdulmajid1m2@gmail.com>
2025-02-26 14:31:36 +0500

    added m code

--- next.config.ts
+++ next.config.ts
@@ -1,49 +1,38 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ['i.pravatar.cc', 'www.google.com', 'wb-uploads.s3.amazonaws.com'],
+//   },
+//   // typescript:{
+//   //   ignoreBuildErrors: true,
+//   // },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+
+// };
+
+
+
+
+// export default nextConfig;
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['i.pravatar.cc', 'www.google.com', 'wb-uploads.s3.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wb-uploads.s3.amazonaws.com',
+      },
+    ],
   },
-  // typescript:{
-  //   ignoreBuildErrors: true,
-  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // webpck:(config)=>{
-  // config.resolve.alias.canvas= false;
-  //return config;
-  //},
-
-  // Add these configurations for NextAuth with reverse proxy
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       headers: [
-  //         {
-  //           key: 'X-Forwarded-Proto',
-  //           value: 'https',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-
-  // // Optional: Add this if you're having issues with the proxy
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/auth/:path*',
-  //       destination: '/api/auth/:path*',
-  //     },
-  //   ];
-  // },
-  
 };
-
-
-
 
 export default nextConfig;
