814320a79ff9a15cb376abf6d74ae34a98f02a05
unknown <abdulmajid1m2@gmail.com>
2025-02-26 14:45:52 +0500

    added m code

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,23 @@
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     domains: ['i.pravatar.cc', 'www.google.com', 'wb-uploads.s3.amazonaws.com'],
-//   },
-//   // typescript:{
-//   //   ignoreBuildErrors: true,
-//   // },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-
-// };
-
-
-
-
-// export default nextConfig;
-
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
+  images: { 
     domains: ['i.pravatar.cc', 'www.google.com', 'wb-uploads.s3.amazonaws.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'wb-uploads.s3.amazonaws.com',
+        pathname: '/public/**',
       },
     ],
+    unoptimized: true, // Consider this option for simple deployment
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  eslint: { 
+    ignoreDuringBuilds: true, 
   },
 };
+
+
+
 
 export default nextConfig;
