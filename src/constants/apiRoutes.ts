09f45601ea4fd9f05ebe18d12bd8c6df61e3d201
amirsuliman01 <amirsuliman01@gmail.com>
2025-03-12 19:48:21 +0500

    base url changed to dev

--- src/constants/apiRoutes.ts
+++ src/constants/apiRoutes.ts
@@ -1,11 +1,11 @@
 // export const BASE_URL = 'https://backend.uat.isaworkbridge.com/'; // for uat.
-// export const BASE_URL = 'https://devbackend.isaworkbridge.com/'; // for development.
-export const BASE_URL = 'https://backend.app.isaworkbridge.com/'; // for production
+export const BASE_URL = 'https://devbackend.isaworkbridge.com/'; // for development.
+// export const BASE_URL = 'https://backend.app.isaworkbridge.com/'; // for production
 
 export const API_ROUTES = {
   // WEBSOCKET_URL: 'wss://uat.app.isaworkbridge.com', // for uat
-  // WEBSOCKET_URL: 'wss://devbackend.isaworkbridge.com', // for development
-  WEBSOCKET_URL: 'wss://backend.app.isaworkbridge.com', // for production
+  WEBSOCKET_URL: 'wss://devbackend.isaworkbridge.com', // for development
+  // WEBSOCKET_URL: 'wss://backend.app.isaworkbridge.com', // for production
   LOGIN: `user/login`,
   GET_USERS: `users`,
   USER_ROLES: `roles`,
