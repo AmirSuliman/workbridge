a9b30ae8abbf3c4e420a2f5b0b9693e1fb7afaa9
unknown <abdulmajid1m2@gmail.com>
2025-03-27 14:43:56 +0500

    adsds

--- src/constants/apiRoutes.ts
+++ src/constants/apiRoutes.ts
@@ -3,7 +3,7 @@ export const BASE_URL = 'https://backend.uat.isaworkbridge.com/'; // for uat.
 // export const BASE_URL = 'https://backend.app.isaworkbridge.com/'; // for production
 
 export const API_ROUTES = {
-  WEBSOCKET_URL: 'wss://uat.app.isaworkbridge.com', // for uat
+  WEBSOCKET_URL: 'wss://backend.uat.isaworkbridge.com', // for uat
   // WEBSOCKET_URL: 'wss://devbackend.isaworkbridge.com', // for development
   // WEBSOCKET_URL: 'wss://backend.app.isaworkbridge.com', // for production
   LOGIN: `user/login`,
