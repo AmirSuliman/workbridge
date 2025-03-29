0161d6db3dcea7b4c0fb1cef36c5da00b4acd8b9
amirsuliman01 <amirsuliman01@gmail.com>
2025-03-29 22:41:42 +0500

    changes

--- src/components/UserInformation/VacationsCard.tsx
+++ src/components/UserInformation/VacationsCard.tsx
@@ -32,7 +32,7 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
   const [vacationDaysUsed, setVacationDaysUsed] = useState(0);
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
-  
+
   const calculateDuration = useCallback(() => {
     if (startDate && endDate) {
       const start = new Date(startDate);
@@ -40,58 +40,65 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
       if (start > end) {
         return 0;
       }
-  
+
       let count = 0;
       let current = new Date(start);
-  
+
       // Loop through each day and only count weekdays
-      while (current < end) { // Change "<=" to "<" to exclude endDate
+      while (current < end) {
+        // Change "<=" to "<" to exclude endDate
         const dayOfWeek = current.getDay();
         if (dayOfWeek !== 0 && dayOfWeek !== 6) {
           count++;
         }
         current.setDate(current.getDate() + 1);
       }
-  
+
       return count > 0 ? count : 0;
     }
     return 0;
   }, [startDate, endDate]);
-  
-  const calculateReturningDate = (start: Date | null, days: number, totalDays: number) => {
+
+  const calculateReturningDate = (
+    start: Date | null,
+    days: number,
+    totalDays: number
+  ) => {
     if (!start || days <= 0 || totalDays <= 0) return null;
-  
+
     let count = 0;
     let current = new Date(start);
-  
+
     while (count < days && count < totalDays) {
       current.setDate(current.getDate() + 1);
       const dayOfWeek = current.getDay();
-  
+
       if (dayOfWeek !== 0 && dayOfWeek !== 6) {
         count++;
       }
     }
-  
+
     return current;
   };
- 
+
   useEffect(() => {
     if (startDate) {
       setVacationDaysUsed(calculateDuration());
     }
   }, [startDate, endDate, calculateDuration]);
-  
-  
+
   useEffect(() => {
-    if (startDate && !endDate) { 
+    if (startDate && !endDate) {
       // Only auto-calculate if endDate hasn't been set by the user
-      const newEndDate = calculateReturningDate(startDate, vacationDaysUsed, totalDays);
+      const newEndDate = calculateReturningDate(
+        startDate,
+        vacationDaysUsed,
+        totalDays
+      );
       setEndDate(newEndDate);
     }
   }, [startDate, vacationDaysUsed, totalDays, endDate]);
-  
-  
+
   const formatDate = (date) => {
     if (!date) return '';
     const day = String(date.getDate()).padStart(2, '0');
@@ -162,23 +169,23 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
 
   return (
     <>
-      <div className="flex items-center justify-between border border-gray-border rounded-[10px] bg-white p-3 md:p-6 md:gap-[3.3rem] w-full">
-        <div className="flex flex-col justify-between gap-[2rem] h-full">
+      <div className='flex items-center justify-between border border-gray-border rounded-[10px] bg-white p-3 md:p-6 md:gap-[3.3rem] w-full'>
+        <div className='flex flex-col justify-between gap-[2rem] h-full'>
           <div>
-            <div className="flex gap-2 items-center mb-2">
-              <div className="flex items-center justify-center rounded-full p-1 bg-[#00B87D]">
-                <UmbrellaIcon classNames="w-4 h-4 text-white" />
+            <div className='flex gap-2 items-center mb-2'>
+              <div className='flex items-center justify-center rounded-full p-1 bg-[#00B87D]'>
+                <UmbrellaIcon classNames='w-4 h-4 text-white' />
               </div>
-              <h3 className="text-dark-navy font-[500] text-sm">
+              <h3 className='text-dark-navy font-[500] text-sm'>
                 Request Vacation
               </h3>
             </div>
-            <p className="font-[400] text-[#878b94] text-xs">
+            <p className='font-[400] text-[#878b94] text-xs'>
               Requests must be made at least 2 weeks prior to submission
             </p>
           </div>
           <button
-            type="button"
+            type='button'
             onClick={handleButtonClick}
             className={`text-white bg-dark-navy py-2 w-[15rem] rounded-[4px] font-[400] text-sm ${
               totalDays === 0 ? 'opacity-50 cursor-not-allowed' : ''
@@ -189,11 +196,11 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
           </button>
         </div>
 
-        <div className="flex flex-col border border-gray-border items-center justify-center rounded-[7px] h-full px-4">
-          <span className="text-lg text-dark-navy font-[400]">
+        <div className='flex flex-col border border-gray-border items-center justify-center rounded-[7px] h-full px-4'>
+          <span className='text-lg text-dark-navy font-[400]'>
             {totalDays ?? 0}
           </span>
-          <span className="text-xs text-dark-navy">days left</span>
+          <span className='text-xs text-dark-navy'>days left</span>
         </div>
       </div>
 
@@ -205,24 +212,24 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
             setEndDate(null);
           }}
         >
-          <div className="p-6 w-full sm:w-[600px]">
-            <div className="flex flex-row items-center gap-2">
+          <div className='p-6 w-full sm:w-[600px]'>
+            <div className='flex flex-row items-center gap-2'>
               <Image
                 loader={imageLoader}
-                src="/vaction.png"
-                alt="img"
+                src='/vaction.png'
+                alt='img'
                 width={40}
                 height={40}
               />
-              <h2 className="text-2xl font-semibold">Request Vacation</h2>
+              <h2 className='text-2xl font-semibold'>Request Vacation</h2>
             </div>
 
             {/* if holidays errors exist then show it as a list */}
             {holidaysErrors.length > 0 && (
-              <section className="my-4 rounded border-2 border-red-500 p-3 relative">
+              <section className='my-4 rounded border-2 border-red-500 p-3 relative'>
                 <button
-                  type="button"
-                  className="absolute -top-2 -right-2 font-medium text-lg border-[1px] border-black bg-white flex items-center justify-center grow-0 shrink-0 rounded-full p-1 size-[20px]"
+                  type='button'
+                  className='absolute -top-2 -right-2 font-medium text-lg border-[1px] border-black bg-white flex items-center justify-center grow-0 shrink-0 rounded-full p-1 size-[20px]'
                   onClick={() => {
                     setHolidaysErrors([]);
                     setStartDate(null);
@@ -231,12 +238,12 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
                 >
                   x
                 </button>
-                <h1 className="font-medium text-lg mb-2">
+                <h1 className='font-medium text-lg mb-2'>
                   Following are the holidays, you cannot make a leave request on
                   these days.
                 </h1>
                 {holidaysErrors.map((holidayError, index) => (
-                  <ul key={index} className="space-y-2 list-disc ml-4">
+                  <ul key={index} className='space-y-2 list-disc ml-4'>
                     <li>
                       {holidayError.title} (
                       {new Date(holidayError.date).toLocaleDateString()})
@@ -246,9 +253,9 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
               </section>
             )}
 
-            <div className="grid grid-cols-2 gap-4 w-full mt-8">
-              <label className="flex flex-col w-full">
-                <span className="text-gray-400 text-[12px]">Leaving Date</span>
+            <div className='grid grid-cols-2 gap-4 w-full mt-8'>
+              <label className='flex flex-col w-full'>
+                <span className='text-gray-400 text-[12px]'>Leaving Date</span>
                 <DatePicker
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
@@ -256,75 +263,81 @@ const VacationsCard = ({ onButtonClick, totalDays }: VacationCardProps) => {
                   startDate={startDate}
                   endDate={endDate}
                   minDate={new Date()}
-                  dateFormat="MM/dd/yyyy"
-                  placeholderText="mm/dd/yyyy"
-                  className="p-3 border rounded w-full"
+                  dateFormat='MM/dd/yyyy'
+                  placeholderText='mm/dd/yyyy'
+                  className='p-3 border rounded w-full'
                   filterDate={(date) => {
                     const day = date.getDay();
-                    return day !== 0 && day !== 6; 
+                    return day !== 0 && day !== 6;
                   }}
                 />
               </label>
 
-              <label className="flex flex-col w-full">
-                <span className="text-gray-400 text-[12px]">
+              <label className='flex flex-col w-full'>
+                <span className='text-gray-400 text-[12px]'>
                   Returning Date
                 </span>
                 <DatePicker
-  selected={endDate}
-  onChange={(date) => setEndDate(date)}
-  selectsEnd
-  startDate={startDate}
-  endDate={endDate}
-  minDate={startDate || undefined}
- maxDate={startDate ? calculateReturningDate(startDate, totalDays, totalDays) || undefined : undefined}
-
-  dateFormat="MM/dd/yyyy"
-  placeholderText="mm/dd/yyyy"
-  className="p-3 border rounded w-full"
-  disabled={!startDate}
-  filterDate={(date) => {
-    const day = date.getDay();
-    return day !== 0 && day !== 6;
-  }}
-/>
-
+                  selected={endDate}
+                  onChange={(date) => setEndDate(date)}
+                  selectsEnd
+                  startDate={startDate}
+                  endDate={endDate}
+                  minDate={startDate || undefined}
+                  maxDate={
+                    startDate
+                      ? calculateReturningDate(
+                          startDate,
+                          totalDays,
+                          totalDays
+                        ) || undefined
+                      : undefined
+                  }
+                  dateFormat='MM/dd/yyyy'
+                  placeholderText='mm/dd/yyyy'
+                  className='p-3 border rounded w-full'
+                  disabled={!startDate}
+                  filterDate={(date) => {
+                    const day = date.getDay();
+                    return day !== 0 && day !== 6;
+                  }}
+                />
               </label>
 
-              <label className="flex flex-col w-full col-span-full">
-                <span className="text-gray-400 text-[12px]">Note</span>
+              <label className='flex flex-col w-full col-span-full'>
+                <span className='text-gray-400 text-[12px]'>Note</span>
                 <textarea
-                  placeholder="Add a message (optional)"
-                  name="note"
+                  placeholder='Add a message (optional)'
+                  name='note'
                   rows={5}
                   value={note}
                   onChange={(e) => setNote(e.target.value)}
-                  className="p-3 border rounded w-full"
+                  className='p-3 border rounded w-full'
                 ></textarea>
               </label>
             </div>
 
-            <div className="h-[1px] w-full bg-gray-200 mt-8" />
+            <div className='h-[1px] w-full bg-gray-200 mt-8' />
 
             {/* Display the vacation duration */}
-            <div className="flex flex-row gap-4 items-center mt-4">
-              <p className="text-[14px]">Vacation days left</p>
-              <div className="text-[14px] border rounded p-3 px-12">
+            <div className='flex flex-row gap-4 items-center mt-4'>
+              <p className='text-[14px]'>Vacation days left</p>
+              <div className='text-[14px] border rounded p-3 px-12'>
                 {vacationDaysUsed} days
               </div>
             </div>
 
-            <div className="flex flex-row  px-6 w-full gap-4 mt-16">
+            <div className='flex flex-row  px-6 w-full gap-4 mt-16'>
               <button
                 onClick={handleRequestVacation}
-                className="mt-4 px-4 py-3 bg-dark-navy text-white rounded w-full"
+                className='mt-4 px-4 py-3 bg-dark-navy text-white rounded w-full'
                 disabled={loading}
               >
                 {loading ? 'Submitting...' : 'Request Vacation'}
               </button>
               <button
                 onClick={() => setIsModalOpen(false)}
-                className="mt-4 px-4 py-3 border rounded w-full"
+                className='mt-4 px-4 py-3 border rounded w-full'
               >
                 Close
               </button>
--- src/components/UserInformation/sickCard.tsx
+++ src/components/UserInformation/sickCard.tsx
@@ -181,26 +181,26 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
 
   return (
     <>
-      <div className="flex items-center justify-between border border-gray-border rounded-[10px] bg-white p-3 md:p-6 md:gap-[3.3rem] w-full">
-        <div className="flex flex-col justify-between gap-[2rem] h-full">
+      <div className='flex items-center justify-between border border-gray-border rounded-[10px] bg-white p-3 md:p-6 md:gap-[3.3rem] w-full'>
+        <div className='flex flex-col justify-between gap-[2rem] h-full'>
           <div>
-            <div className="flex gap-2 items-center mb-2">
-              <div className="flex items-center justify-center rounded-full p-1">
+            <div className='flex gap-2 items-center mb-2'>
+              <div className='flex items-center justify-center rounded-full p-1'>
                 <Image
                   loader={imageLoader}
-                  src="/sickleave.png"
-                  alt="img"
+                  src='/sickleave.png'
+                  alt='img'
                   width={25}
                   height={25}
                 />
               </div>
-              <h3 className="text-dark-navy font-[500] text-sm">
+              <h3 className='text-dark-navy font-[500] text-sm'>
                 Request Sick Leave
               </h3>
             </div>
           </div>
           <button
-            type="button"
+            type='button'
             onClick={handleButtonClick}
             className={`text-white bg-dark-navy py-2 w-[15rem] rounded-[4px] font-[400] text-sm ${
               totalDays === 0 ? 'opacity-50 cursor-not-allowed' : ''
@@ -211,11 +211,11 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
           </button>
         </div>
 
-        <div className="flex flex-col border border-gray-border items-center justify-center rounded-[7px] h-full px-4">
-          <span className="text-lg text-dark-navy font-[400]">
+        <div className='flex flex-col border border-gray-border items-center justify-center rounded-[7px] h-full px-4'>
+          <span className='text-lg text-dark-navy font-[400]'>
             {totalDays ?? 0}
           </span>
-          <span className="text-xs text-dark-navy">days left</span>
+          <span className='text-xs text-dark-navy'>days left</span>
         </div>
       </div>
 
@@ -227,23 +227,23 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
             setEndDate(null);
           }}
         >
-          <div className="p-6 w-full sm:w-[600px]">
-            <div className="flex flex-row items-center gap-2">
+          <div className='p-6 w-full sm:w-[600px]'>
+            <div className='flex flex-row items-center gap-2'>
               <Image
                 loader={imageLoader}
-                src="/sickleave.png"
-                alt="img"
+                src='/sickleave.png'
+                alt='img'
                 width={40}
                 height={40}
               />
-              <h2 className="text-2xl font-semibold">Request Sick Leave</h2>
+              <h2 className='text-2xl font-semibold'>Request Sick Leave</h2>
             </div>
             {/* if holidays errors exist then show it as a list */}
             {holidaysErrors.length > 0 && (
-              <section className="my-4 rounded border-2 border-red-500 p-3 relative">
+              <section className='my-4 rounded border-2 border-red-500 p-3 relative'>
                 <button
-                  type="button"
-                  className="absolute -top-2 -right-2 font-medium text-lg border-[1px] border-black bg-white flex items-center justify-center grow-0 shrink-0 rounded-full p-1 size-[20px]"
+                  type='button'
+                  className='absolute -top-2 -right-2 font-medium text-lg border-[1px] border-black bg-white flex items-center justify-center grow-0 shrink-0 rounded-full p-1 size-[20px]'
                   onClick={() => {
                     setHolidaysErrors([]);
                     setStartDate(null);
@@ -252,12 +252,12 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                 >
                   x
                 </button>
-                <h1 className="font-medium text-lg mb-2">
+                <h1 className='font-medium text-lg mb-2'>
                   Following are the holidays, you cannot make a leave request on
                   these days.
                 </h1>
                 {holidaysErrors.map((holidayError, index) => (
-                  <ul key={index} className="space-y-2 list-disc ml-4">
+                  <ul key={index} className='space-y-2 list-disc ml-4'>
                     <li>
                       {holidayError.title} (
                       {new Date(holidayError.date).toLocaleDateString()})
@@ -266,9 +266,9 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                 ))}
               </section>
             )}
-            <div className="grid grid-cols-2 gap-4 w-full mt-8">
-              <label className="flex flex-col w-full">
-                <span className="text-gray-400 text-[12px]">Leaving Date</span>
+            <div className='grid grid-cols-2 gap-4 w-full mt-8'>
+              <label className='flex flex-col w-full'>
+                <span className='text-gray-400 text-[12px]'>Leaving Date</span>
                 <DatePicker
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
@@ -276,9 +276,9 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                   startDate={startDate}
                   endDate={endDate}
                   minDate={new Date()}
-                  dateFormat="MM/dd/yyyy"
-                  placeholderText="mm/dd/yyyy"
-                  className="p-3 border rounded w-full"
+                  dateFormat='MM/dd/yyyy'
+                  placeholderText='mm/dd/yyyy'
+                  className='p-3 border rounded w-full'
                   filterDate={(date) => {
                     const day = date.getDay();
                     return day !== 0 && day !== 6;
@@ -286,8 +286,8 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                 />
               </label>
 
-              <label className="flex flex-col w-full">
-                <span className="text-gray-400 text-[12px]">
+              <label className='flex flex-col w-full'>
+                <span className='text-gray-400 text-[12px]'>
                   Returning Date
                 </span>
                 <DatePicker
@@ -306,9 +306,9 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                         ) || undefined
                       : undefined
                   }
-                  dateFormat="MM/dd/yyyy"
-                  placeholderText="mm/dd/yyyy"
-                  className="p-3 border rounded w-full"
+                  dateFormat='MM/dd/yyyy'
+                  placeholderText='mm/dd/yyyy'
+                  className='p-3 border rounded w-full'
                   disabled={!startDate}
                   filterDate={(date) => {
                     const day = date.getDay();
@@ -316,25 +316,25 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                   }}
                 />
               </label>
-              <label className="flex flex-col w-full col-span-full">
-                <span className="text-gray-400 text-[12px]">Note</span>
+              <label className='flex flex-col w-full col-span-full'>
+                <span className='text-gray-400 text-[12px]'>Note</span>
                 <textarea
-                  placeholder="Add a message (optional)"
-                  name="note"
+                  placeholder='Add a message (optional)'
+                  name='note'
                   rows={5}
                   value={note}
                   onChange={(e) => setNote(e.target.value)}
-                  className="p-3 border rounded w-full"
+                  className='p-3 border rounded w-full'
                 ></textarea>
               </label>
             </div>
 
-            <div className="h-[1px] w-full bg-gray-200 mt-8" />
+            <div className='h-[1px] w-full bg-gray-200 mt-8' />
 
             {/* Display the vacation duration */}
-            <div className="flex flex-row gap-4 items-center mt-4">
-              <p className="text-[14px]">Sick days left</p>
-              <div className="text-[14px] border rounded p-3 px-12">
+            <div className='flex flex-row gap-4 items-center mt-4'>
+              <p className='text-[14px]'>Sick days left</p>
+              <div className='text-[14px] border rounded p-3 px-12'>
                 {vacationDaysUsed} days
               </div>
             </div>
@@ -344,7 +344,7 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
               setSelectedFiles={setSelectedFiles}
             />
 
-            <div className="flex flex-row  px-6 w-full gap-4 mt-16">
+            <div className='flex flex-row  px-6 w-full gap-4 mt-16'>
               {/* <button
                 type="button"
                 onClick={handleRequestVacation}
@@ -354,14 +354,14 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
                 {loading ? 'Submitting...' : 'Request Sick Leave'}
               </button> */}
               <button
-                type="button"
+                type='button'
                 onClick={handleRequestVacation}
-                className="mt-4 px-4 py-3 bg-dark-navy text-white rounded w-full"
+                className='mt-4 px-4 py-3 bg-dark-navy text-white rounded w-full'
                 disabled={loading}
               >
                 {loading ? (
-                  <span className="flex items-center justify-center gap-2">
-                    <BiLoaderCircle className="h-5 w-5 animate-spin" />
+                  <span className='flex items-center justify-center gap-2'>
+                    <BiLoaderCircle className='h-5 w-5 animate-spin' />
                   </span>
                 ) : (
                   'Request Sick Leave'
@@ -369,9 +369,9 @@ const SickCard = ({ onButtonClick, totalDays }: SickCardProps) => {
               </button>
               <button
                 disabled={loading}
-                type="button"
+                type='button'
                 onClick={() => setIsModalOpen(false)}
-                className="mt-4 px-4 py-3 border rounded w-full"
+                className='mt-4 px-4 py-3 border rounded w-full'
               >
                 Close
               </button>
