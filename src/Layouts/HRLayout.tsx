6274563daa4fed0637c4eaae700621203e73b82e
amirsuliman01 <amirsuliman01@gmail.com>
2025-04-15 15:12:32 +0500

    user sidber toggle fixed

--- src/Layouts/HRLayout.tsx
+++ src/Layouts/HRLayout.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import HrHeader from '../components/Headers/HrHeader';
 import HrSidebar from '../components/SidebarMenu/HrSidebar';
 
 const HRLayout = ({ children }: { children: React.ReactNode }) => {
+  const [showSidbarMenu, setShowSidebarMenu] = useState(false);
+
   return (
-    <main className="flex">
+    <main className='flex'>
       <HrSidebar />
-      <div className="ml-0 lg:ml-[270px] w-full">
+      <div className='ml-0 lg:ml-[270px] w-full'>
         <HrHeader />
-        <div className="p-4 2xl:p-8">{children}</div>
+        <div className='p-4 2xl:p-8'>{children}</div>
       </div>
     </main>
   );
